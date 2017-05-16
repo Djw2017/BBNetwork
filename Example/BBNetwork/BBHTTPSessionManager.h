127b86cd04f1cbb7648baed1b961775853209dfe
Dong JW <1971728089@qq.com>
2017-05-16 17:05:52 +0800

    添加pod

---
+++ Example/BBNetwork/BBHTTPSessionManager.h
@@ -0,0 +1,13 @@
+//
+//  BBHTTPSessionManager.h
+//  BBNetwork
+//
+//  Created by Dongjw on 17/5/16.
+//  Copyright © 2017年 Babybus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BBHTTPSessionManager : NSObject
+
+@end
---
+++ Example/BBNetwork/BBHTTPSessionManager.m
@@ -0,0 +1,13 @@
+//
+//  BBHTTPSessionManager.m
+//  BBNetwork
+//
+//  Created by Dongjw on 17/5/16.
+//  Copyright © 2017年 Babybus. All rights reserved.
+//
+
+#import "BBHTTPSessionManager.h"
+
+@implementation BBHTTPSessionManager
+
+@end
